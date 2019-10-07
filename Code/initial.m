3598c292aa61facf3ef85b8e78c876ff67fea404
Faezeh <fhaghverd@gmail.com>
2019-10-07 14:15:48 +0330

    UPLOAD FILES

---
+++ Code/initial.m
@@ -0,0 +1,12 @@
+clear all;
+clc;
+global Ena Ek El gl ri ro dz Cm Vr;
+Ena=115; %mV
+Ek=-12; %mV
+El=10.6; %mV
+gl=0.3; %mS/cm2
+ri=10;ro=10; %ohm/cm
+dz=1; %cm
+Cm=1; %microF/cm2
+Vr=80; %mV
+sim HW2sim
